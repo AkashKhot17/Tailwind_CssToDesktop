f830f6e72b0f24e4e066d229de86f5660f1b1f6f
Akashkhot17 <akkikhot2308@gmail.com>
2025-01-28 22:17:16 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+
+      colors: {
+        'aka': '#ee0181',
+        'primary': '#3238f2'
+
+      },
+
+      fontFamily: {
+        'aka-font': ['Poppins','sans-sarif'],
+        'display': ['Poppins','sans-sarif'],
+          'body': ['Inter','sans-sarif'],
+      }
+    },
+  },
+  plugins: [],
+}
